8685b9862393e507f14f343b71e2cefd1534d2b6
Shyaar Sati Fwatshak <104396521+Shyaar@users.noreply.github.com>
2024-12-06 13:53:25 +0100

    Update post.jsx

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -19,7 +19,7 @@ const Post = () => {
         <div className="userData border rounded-md p-4 shadow-md">
             <h3></h3>
             <hr />
-            <p>{postMessage}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, alias praesentium quibusdam magni id sequi itaque, excepturi dolorem delectus voluptas facere officiis vel quasi saepe numquam, necessitatibus dolores distinctio maxime velit. Est, officiis optio, accusantium ex, neque quasi totam incidunt dolorem amet adipisci sunt natus cum minus pariatur quos voluptatum!</p>
             <hr />
             <div className='flex'>
                 <button className='m-4'>bookmark</button>
